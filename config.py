1b6eac5668c8130d48517007ecd78895d95c8bf5
Logan Cusano <logan@simplestepsolutions.com>
2025-04-18 15:16:14 -0400

    Updated config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # --- Configuration ---
-APP_NAME = "Simple Step Solutions - Win11 Readiness Check"
+APP_NAME = "Simple Step Solutions - Win11 Upgrade Readiness Assessment"
 LOGO_FILENAME = "logo.png"
 ICON_FILENAME = "logo.ico"
 # --- !!! REPLACE WITH YOUR ACTUAL API ENDPOINT !!! ---
-API_ENDPOINT_URL = "https://n8n.simplestep.tech/webhook-test/aecfd3e4-659b-4e2c-a87e-3b13b34267a0"
+API_ENDPOINT_URL = "https://n8n.simplestep.tech/webhook/aecfd3e4-659b-4e2c-a87e-3b13b34267a0"
 # --- !!! REPLACE WITH YOUR SUPPORT PHONE NUMBER !!! ---
 SUPPORT_PHONE_NUMBER = "1-914-250-9190"
 # --- End Configuration ---
